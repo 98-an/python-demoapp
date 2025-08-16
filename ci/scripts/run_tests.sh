ab34864e8663c38ddc7360d71806f0a9641c495a
Anakar Yassin <139815981+98-an@users.noreply.github.com>
2025-08-17 00:32:01 +0100

    Update run_tests.sh

--- ci/scripts/run_tests.sh
+++ ci/scripts/run_tests.sh
@@ -1,1 +1,9 @@
-h
+#!/usr/bin/env bash
+set -eux
+REQ_PATH="${REQ_PATH:-src/requirements.txt}"
+python --version
+pip install --upgrade pip
+pip install -r "$REQ_PATH"
+pip install pytest bandit
+pytest -q || true
+bandit -q -r src || true
