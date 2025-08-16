d4afc4ef0c20dbac242b3e42c922510bb0499542
Anakar Yassin <139815981+98-an@users.noreply.github.com>
2025-08-17 00:33:55 +0100

    Update docker_build.sh

--- ci/scripts/docker_build.sh
+++ ci/scripts/docker_build.sh
@@ -1,1 +1,7 @@
-
+#!/usr/bin/env bash
+set -eux
+DOCKERFILE="${DOCKERFILE:-container/Dockerfile}"
+IMAGE_REPO="${IMAGE_REPO:-ghcr.io/98-an/python-demoapp}"
+SHORT_SHA="${SHORT_SHA:-local}"
+docker build -f "$DOCKERFILE" -t "$IMAGE_REPO:$SHORT_SHA" .
+docker tag "$IMAGE_REPO:$SHORT_SHA" "$IMAGE_REPO:latest"
