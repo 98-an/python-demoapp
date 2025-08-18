19d1593aaea053253695fa0643b3b80824e5b26f
Anakar Yassin <139815981+98-an@users.noreply.github.com>
2025-08-18 21:18:13 +0100

    Update run.py

--- src/run.py
+++ src/run.py
@@ -9,6 +9,16 @@ if __name__ == "__main__":
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.run(host="0.0.0.0", port=port)
 
+import subprocess, hashlib, yaml
+
+def demo_cmd(cmd: str) -> str:
+    return subprocess.check_output(cmd, shell=True, text=True)  # nosec
+
+def demo_md5(data: str) -> str:
+    return hashlib.md5(data.encode()).hexdigest()  # nosec
+
+def demo_yaml(s: str):
+    return yaml.load(s, Loader=yaml.Loader)  # nosec
 
 
 
