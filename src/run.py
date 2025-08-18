ae97a2db0d2665ee2a8eb26061129b4fafbfecbd
Anakar Yassin <139815981+98-an@users.noreply.github.com>
2025-08-18 20:46:51 +0100

    Update run.py

--- src/run.py
+++ src/run.py
@@ -10,19 +10,5 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
 
 
-import subprocess  
-import hashlib     
-import yaml        
-def _vuln_cmd(cmd: str) -> str:
-  
-    return subprocess.check_output(cmd, shell=True, text=True)  
-
-def _vuln_md5(data: str) -> str:
-    
-    return hashlib.md5(data.encode()).hexdigest()  
-
-def _vuln_yaml_load(s: str):
-   =
-    return yaml.load(s, Loader=yaml.Loader)  
 
 
